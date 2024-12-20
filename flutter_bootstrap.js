eafa5f5c4a0a9cc7c24a8ccc590fae7d8030eb19
Origin Tech <120145954+Origin-Tech-000@users.noreply.github.com>
2024-12-20 18:51:42 +0530

    testing connection

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2883778193"
+    serviceWorkerVersion: "1344590933"
   }
 });
